6ff617db6c601efedea3def8219900223384c718
Eduardo Falcão <eduardolfalcao@gmail.com>
2019-10-21 22:37:48 -0300

    Add put, post and delete to person service

--- src/main/java/br/com/edufalcao/model/Person.java
+++ src/main/java/br/com/edufalcao/model/Person.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 public class Person implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+	
 	private long id;
 	private String firstName, lastName;
 	private Date birth;
--- src/main/java/br/com/edufalcao/services/PersonServices.java
+++ src/main/java/br/com/edufalcao/services/PersonServices.java
@@ -1,6 +1,8 @@
 package br.com.edufalcao.services;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -12,6 +14,20 @@ public class PersonServices {
 
 	private final AtomicLong counter = new AtomicLong();
 	
+	public Person create(Person p) {
+		//insert person into database
+		return p;
+	}
+	
+	public Person update(Person p) {
+		//update person in database
+		return p;
+	}
+	
+	public void delete(String id) {
+		// removes from database
+	}
+	
 	public Person findById(String id) {
 		Person p = new Person();
 		p.setId(counter.incrementAndGet());
@@ -22,6 +38,15 @@ public class PersonServices {
 		p.setBirth(cal.getTime());
 		p.setAddress("Catolé, Campina Grande");
 		return p;
+	}
+	
+	public List<Person> findAll(){
+		List<Person> persons = new ArrayList<Person>();
+		for(int i = 0; i < 10; i++) {
+			Person p = findById("");
+			persons.add(p);
+		}
+		return persons;
 	}
 	
 }
